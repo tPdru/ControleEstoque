21c7ae0c0eed9a7b8167b4d8f3b71955b9c6f16b
tPdru <pedroteixeiradeandrade@gmail.com>
2024-11-14 21:45:23 -0300

    logo 1

--- app/src/main/java/br/com/etecia/appcontroledeestoque/TelaLoginActivity.java
+++ app/src/main/java/br/com/etecia/appcontroledeestoque/TelaLoginActivity.java
@@ -15,10 +15,6 @@ public class TelaLoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.tela_login_layout);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
